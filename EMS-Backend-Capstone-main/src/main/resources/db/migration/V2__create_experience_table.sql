7c02e152ce1c311dd4a495c2208084f6997eb68f
madhavc9 <ranmadhav@gmail.com>
2025-12-06 14:35:08 +0530

    .

--- EMS-Backend-Capstone-main/src/main/resources/db/migration/V2__create_experience_table.sql
+++ EMS-Backend-Capstone-main/src/main/resources/db/migration/V2__create_experience_table.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS experience (
     tech_stack VARCHAR(100) NOT NULL,
     company VARCHAR(100),
     years INT NOT NULL CHECK (years > 0),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
     CONSTRAINT fk_experience_employee
         FOREIGN KEY (employee_id)
         REFERENCES employees(id)
